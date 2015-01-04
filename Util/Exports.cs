97c3ec2c89683cbe01d9f1e0fefcd045d9c034fe
macromaniac <joeycaero@gmail.com>
2015-01-04 09:27:02 -0800

    added in method that lets you find the current project directory

--- Util/Exports.cs
+++ Util/Exports.cs
@@ -6,15 +6,24 @@ using System.Threading.Tasks;
 
 using System.Runtime.InteropServices;
 using System.Diagnostics;
-namespace XUtil {
-	public static class ExtensionMethods {
+using System.IO;
 
+namespace XUtil {
+	public static class Meta {
+		public static string ProjectDirectory() {
+			return Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+		}
+	}
+	public static class ExtensionMethods {
 		//This makes sure that a process dies when the parent process dies
 		static Job j = null;
-		public static void KillProcessOnExit(this Process p){
+		public static void KillProcessOnExit(this Process p) {
 			if (j == null)
 				j = new Job();
 			j.AddProcess(p.Id);
 		}
+
+
+
 	}
 }
