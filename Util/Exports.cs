274ec0de14065672753c5cd4a1172515f153c456
macromaniac <joeycaero@gmail.com>
2015-01-04 08:44:30 -0800

    Added proceess utility function

---
+++ Util/Exports.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+namespace XUtil {
+	public static class ExtensionMethods {
+
+		//This makes sure that a process dies when the parent process dies
+		static Job j = null;
+		public static void KillProcessOnExit(this Process p){
+			if (j == null)
+				j = new Job();
+			j.AddProcess(p.Id);
+		}
+	}
+}
