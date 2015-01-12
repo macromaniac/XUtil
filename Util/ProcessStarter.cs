55078ab99d54b7788ad6a54db84c9f3136a1f087
macromaniac <joeycaero@gmail.com>
2015-01-12 14:40:09 -0600

    Process start now works for PATH processes

--- Util/ProcessStarter.cs
+++ Util/ProcessStarter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,13 +9,29 @@ using System.Threading.Tasks;
 namespace XUtil {
 	internal static class ProcessStarter {
 		internal static string ProcessOutput(string fileName, string args) {
+			if (File.Exists(fileName) == false) {
+				string pathFile = "";
+				//if we can't find the file we will search the PATH enviro var
+				string varsString = Environment.GetEnvironmentVariable("PATH");
+				string[] vars = varsString.Split(';');
+				foreach (string dirString in vars) {
+					if (File.Exists(dirString + @"\" + fileName))
+						pathFile = dirString + @"\" + fileName;
+					if (File.Exists(dirString + @"\" + fileName + ".exe"))
+						pathFile = dirString + @"\" + fileName + ".exe";
+				}
+				if (pathFile == "")
+					throw new FileNotFoundException(
+						"Could not find " + fileName + " in PATH or by itself");
+			}
 			Process p = new Process();
 			p.StartInfo.FileName = fileName;
 			p.StartInfo.Arguments = args;
+			//can't redirect output if we use the shell, otherwise calling
+			//path programs would be easy
 			p.StartInfo.UseShellExecute = false;
 			p.StartInfo.RedirectStandardOutput = true;
 			p.Start();
-
 			string output = p.StandardOutput.ReadToEnd();
 			p.WaitForExit();
 			return output;
---
+++ XUtilTester/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using XUtil;
+
+namespace XUtilTester {
+	class Program {
+		static void Main(string[] args) {
+			string output = XUtil.Meta.OutputFromProcess(
+				"python", @"C:\Users\macro\git\Blocker\BlockerBuild\BlockerBuild.py");
+
+			Console.WriteLine(output);
+			Console.ReadKey();
+		}
+	}
+}
